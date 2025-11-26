3b6d41bb6d628cf3428a64478288af3336fede58
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2025-11-26 17:45:01 -0300

    [FIX] Removendo letra maiúscula na referenciação do animalControler

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import AnimalController from "../controllers/AnimalController.js";
+import AnimalController from "../controllers/animalController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import upload from "../config/multerConfig.js";
 
